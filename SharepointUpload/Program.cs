136f064a69ed24afe4b15b51c66643c187a47527
Karl Strauss <kstrauss@realgy.com>
2016-10-11 17:47:13 -0400

    Add project files.

---
+++ SharepointUpload/Program.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contoso.Core.LargeFileUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Request Office365 site from the user
+            string siteUrl = args[0];
+
+            /* Prompt for Credentials */
+            Console.WriteLine("Credentials for {0}", siteUrl);
+
+            string userName = args[1];
+            SecureString pwd = new SecureString();
+            foreach(var c in args[2]){
+                pwd.AppendChar(c);
+            }
+
+            /* End Program if no Credentials */
+            if (string.IsNullOrEmpty(userName) || (pwd == null))
+                return;
+
+            ClientContext ctx = new ClientContext(siteUrl);
+            ctx.AuthenticationMode = ClientAuthenticationMode.Default;
+            ctx.Credentials = new SharePointOnlineCredentials(userName, pwd);
+            try
+            {
+                new FileUploadService().UploadFileSlicePerSlice(ctx, args[4], args[3], createLibrary: false);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Exception while uploading files to the target site: {0}.", ex.ToString()));
+            }
+        }
+
+        static SecureString GetPassword()
+        {
+            SecureString sStrPwd = new SecureString();
+            try
+            {
+                Console.Write("Password: ");
+
+                for (ConsoleKeyInfo keyInfo = Console.ReadKey(true); keyInfo.Key != ConsoleKey.Enter; keyInfo = Console.ReadKey(true))
+                {
+                    if (keyInfo.Key == ConsoleKey.Backspace)
+                    {
+                        if (sStrPwd.Length > 0)
+                        {
+                            sStrPwd.RemoveAt(sStrPwd.Length - 1);
+                            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                            Console.Write(" ");
+                            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        }
+                    }
+                    else if (keyInfo.Key != ConsoleKey.Enter)
+                    {
+                        Console.Write("*");
+                        sStrPwd.AppendChar(keyInfo.KeyChar);
+                    }
+
+                }
+                Console.WriteLine("");
+            }
+            catch (Exception e)
+            {
+                sStrPwd = null;
+                Console.WriteLine(e.Message);
+            }
+
+            return sStrPwd;
+        }
+
+        static string GetUserName()
+        {
+            string strUserName = string.Empty;
+            try
+            {
+                Console.Write("Username: ");
+                strUserName = Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                strUserName = string.Empty;
+            }
+            return strUserName;
+        }
+
+        static string GetSite()
+        {
+            string siteUrl = string.Empty;
+            try
+            {
+                Console.Write("Enter your Office365 site collection URL: ");
+                siteUrl = Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                siteUrl = string.Empty;
+            }
+            return siteUrl;
+        }
+    }
+   
+}
